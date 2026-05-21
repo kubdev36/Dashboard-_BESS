4562d9d24635f37f55d1105c5f5ac14853c7a626
Như Trúc <nhutruc11111@gmail.com>
2026-05-21 14:43:25 +0700

    index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import "./components/i18n/i18n";
 import { LanguageProvider } from './components/Lang/LanguageProvider'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
